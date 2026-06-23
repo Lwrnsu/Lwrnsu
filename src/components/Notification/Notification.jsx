1825b39d57a0c0ec637b14ba089831c80f07c0ae
Lwrnsu <jaylawrencedeluna.2007@gmail.com>
2026-06-23 17:12:19 +0800

    minor fix in details. added: timeout in notification component.

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,15 +1,24 @@
 import styles from './Notification.module.css';
 
-import { useState } from 'react';
-function Notification({ status, error }) {
+import { useState, useEffect } from 'react';
+function Notification({ status, error, message = "Success!" }) {
     const [isClosed, setIsClosed] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsClosed(true);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <aside className={isClosed ? styles.hide : styles.notification}>
             <div className={styles.notificationHeader}>
                 <h6 className={styles.notificationCode}>STATUS CODE: {status}</h6>
                 <button className={styles.notificationClose} onClick={() => setIsClosed(true)}>X</button>
             </div>
-            <p className={styles.notificationMessage}>{error ? error : "Success!"}</p>
+            <p className={styles.notificationMessage}>{error ? error : message}</p>
         </aside>
     )
 }
